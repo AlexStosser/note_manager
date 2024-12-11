c17a1b4d2ce6059c58960b14f4d229fe9fb19ac5
AlexStosser <alexandertolokoncev@gmail.com>
2024-12-11 23:42:06 +0300

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,9 @@
+username = "Александр"
+title = 'Grade 1.1'
+content = 'Практическое задание 1'
+status = 'В процессе'
+created_date = '11-12-2024'
+issue_date = '12-12-2024'
+print("Александр:", username)
+print('Grade 1.1:', title)
+print('Практическое задание 1:', content)
